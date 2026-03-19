929028bae7467cd997785803c5039db1e4350702
foreveryoung2023car <foreveryoung2023car@gmail.com>
2026-03-19 12:48:36 +0800

    Update config.js

--- 2026F1/config.js
+++ 2026F1/config.js
@@ -1,1 +1,1 @@
-const API = "https://script.google.com/macros/s/AKfycbzu_5oYwv98extikVU0RvC0mwKaGAIZakdFZMT8VLDjm2z5ksFTfT3qjgSt0wLEpFwS/exec";
+const API = "https://script.google.com/macros/s/AKfycbxMsnEwMk1u9ahSj8t-CqyUzhcMr4HgqHhdcInqd8oEiX-wt2qfxpZu9Zzq2muVcIP8/exec";
