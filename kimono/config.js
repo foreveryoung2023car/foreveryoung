4d953771213b334efed6a95338bad9ea98f4566b
foreveryoung2023car <foreveryoung2023car@gmail.com>
2026-04-21 21:27:06 +0800

    Update config.js

--- kimono/config.js
+++ kimono/config.js
@@ -8,7 +8,7 @@ const KIMONO_CONFIG = {
 
     // ★ Google Apps Script Web App URL
     // 重新部署後只需要改這裡
-    APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxYSgei9nFBiIoQoKlMkMfBGdUdxmzE9XpJG3sFpGz28IF3FNTgKj6jFvZ_3yZeNSLx/exec',
+    APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyG-7aTPn7RKlMTltxfddQUnTnPAUAU-uUsvbtWghJs2qJpIohpL8oe1IXrje0njNCS/exec',
 
     // ★ 聯繫連結
     LINE_URL:       'https://lin.ee/TgFCvYQ',
