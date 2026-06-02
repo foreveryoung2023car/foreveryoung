acefc2656593e173f0115c586ac41b981d49c4cc
Ciro <chenciro91@gmail.com>
2026-06-03 03:33:52 +0900

    fix: deploy only static site to GitHub Pages

--- kimono-system/firebase/functions/lib/index.js
+++
@@ -1,5 +0,0 @@
-export { createPublicOrder, transitionOrder } from "./http/orders.js";
-export { requestRefund, requestRefundByStaff } from "./http/refunds.js";
-export { checkInOrder, checkInOrderByStaff } from "./http/checkins.js";
-export { getAuditLogs } from "./http/audit.js";
-//# sourceMappingURL=index.js.map
